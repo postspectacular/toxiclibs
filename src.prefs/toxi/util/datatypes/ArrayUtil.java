846576d2485a6d507ff86c639eccfadc344872e8
Karsten Schmidt <k@postspectacular.com>
2009-02-19 22:15:28 +0000

    added indexInArray methods to ArrayUtil, updated changelog
    --HG--
    branch : toxiclibs

--- src.prefs/toxi/util/datatypes/ArrayUtil.java
+++ src.prefs/toxi/util/datatypes/ArrayUtil.java
@@ -12,6 +12,172 @@ import java.util.Random;
 public class ArrayUtil {
 
 	/**
+	 * Adds all array elements to the given collection of the same type.
+	 * 
+	 * @param <T>
+	 * @param array
+	 *            array
+	 * @param collection
+	 *            existing collection or null (to create a new {@link ArrayList}
+	 *            automatically)
+	 */
+	public static <T> void addArrayToCollection(T[] array,
+			Collection<T> collection) {
+		if (collection == null) {
+			collection = new ArrayList<T>();
+		}
+		for (T o : array) {
+			collection.add(o);
+		}
+	}
+
+	/**
+	 * Converts the generic array into an {@link ArrayList} of the same type.
+	 * 
+	 * @param array
+	 * @return array list version
+	 */
+	public static <T> ArrayList<T> arrayToList(T[] array) {
+		ArrayList<T> list = new ArrayList<T>(array.length);
+		for (T element : array) {
+			list.add(element);
+		}
+		return list;
+	}
+
+	/**
+	 * Returns the index of the element where the given value is found in the
+	 * array.
+	 * 
+	 * @param needle
+	 *            number to find
+	 * @param stack
+	 *            array to search
+	 * @param maxLen
+	 *            number of elements to search
+	 * @return array index or -1 if value couldn't be found in array
+	 */
+	public static int indexInArray(float needle, float[] stack, int maxLen) {
+		for (int i = 0; i < maxLen; i++) {
+			if (stack[i] == needle) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * Returns the index of the element where the given value is found in the
+	 * array.
+	 * 
+	 * @param needle
+	 *            number to find
+	 * @param stack
+	 *            array to search
+	 * @param maxLen
+	 *            number of elements to search
+	 * @return array index or -1 if value couldn't be found in array
+	 */
+	public static int indexInArray(int needle, int[] stack, int maxLen) {
+		for (int i = 0; i < maxLen; i++) {
+			if (stack[i] == needle) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * Reverses the item order of the supplied byte array.
+	 * 
+	 * @param array
+	 */
+	public static void reverse(byte[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			byte tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
+	 * Reverses the item order of the supplied char array.
+	 * 
+	 * @param array
+	 */
+	public static void reverse(char[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			char tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
+	 * Reverses the item order of the supplied float array.
+	 * 
+	 * @param array
+	 */
+	public static void reverse(float[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			float tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
+	 * Reverses the item order of the supplied int array.
+	 * 
+	 * @param array
+	 */
+	public static void reverse(int[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			int tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
+	 * Reverses the item order of the supplied short array.
+	 * 
+	 * @param array
+	 */
+	public static void reverse(short[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			short tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
+	 * Reverses the item order of the supplied array (generic types).
+	 * 
+	 * @param array
+	 */
+	public static <T> void reverse(T[] array) {
+		int len = array.length - 1;
+		int len2 = array.length / 2;
+		for (int i = 0; i < len2; i++) {
+			T tmp = array[i];
+			array[i] = array[len - i];
+			array[len - i] = tmp;
+		}
+	}
+
+	/**
 	 * Rearranges the array items in random order using the default
 	 * java.util.Random generator. Operation is in-place, no copy is created.
 	 * 
@@ -37,128 +203,4 @@ public class ArrayUtil {
 			array[r] = swap;
 		}
 	}
-
-	/**
-	 * Reverses the item order of the supplied array (generic types).
-	 * 
-	 * @param array
-	 */
-	public static <T> void reverse(T[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			T tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Reverses the item order of the supplied byte array.
-	 * 
-	 * @param array
-	 */
-	public static void reverse(byte[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			byte tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Reverses the item order of the supplied short array.
-	 * 
-	 * @param array
-	 */
-	public static void reverse(short[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			short tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Reverses the item order of the supplied char array.
-	 * 
-	 * @param array
-	 */
-	public static void reverse(char[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			char tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Reverses the item order of the supplied int array.
-	 * 
-	 * @param array
-	 */
-	public static void reverse(int[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			int tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Reverses the item order of the supplied float array.
-	 * 
-	 * @param array
-	 */
-	public static void reverse(float[] array) {
-		int len = array.length - 1;
-		int len2 = array.length / 2;
-		for (int i = 0; i < len2; i++) {
-			float tmp = array[i];
-			array[i] = array[len - i];
-			array[len - i] = tmp;
-		}
-	}
-
-	/**
-	 * Converts the generic array into an {@link ArrayList} of the same type.
-	 * 
-	 * @param array
-	 * @return array list version
-	 */
-	public static <T> ArrayList<T> arrayToList(T[] array) {
-		ArrayList<T> list = new ArrayList<T>(array.length);
-		for (int i = 0; i < array.length; i++) {
-			list.add(array[i]);
-		}
-		return list;
-	}
-
-	/**
-	 * Adds all array elements to the given collection of the same type.
-	 * 
-	 * @param <T>
-	 * @param array
-	 *            array
-	 * @param collection
-	 *            existing collection or null (to create a new {@link ArrayList}
-	 *            automatically)
-	 */
-	static <T> void addArrayToCollection(T[] array, Collection<T> collection) {
-		if (collection == null) {
-			collection = new ArrayList<T>();
-		}
-		for (T o : array) {
-			collection.add(o);
-		}
-	}
-
 }
