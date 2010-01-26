eda5c8d0c70f9b608eb1ec42de89f883a0a8614f
Karsten Schmidt <k@postspectacular.com>
2010-01-26 13:14:27 +0000

    adding tests for new UnitTranslator methods
    --HG--
    branch : toxiclibs

---
+++ src.test/toxi/test/math/UnitTransTest.java
@@ -0,0 +1,15 @@
+package toxi.test.math;
+
+import junit.framework.TestCase;
+import toxi.math.conversion.UnitTranslator;
+
+public class UnitTransTest extends TestCase {
+
+    public void testAreaConverters() {
+        double area =
+                UnitTranslator.POINT_POSTSCRIPT
+                        * UnitTranslator.POINT_POSTSCRIPT;
+        assertEquals(1d, UnitTranslator.squarePointsToInch(area));
+        assertEquals(645.16, UnitTranslator.squarePointsToMillis(area));
+    }
+}
