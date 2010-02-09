5396b19bd564dd0dd99cffa714901c6a4c89062e
Karsten Schmidt <k@postspectacular.com>
2010-02-09 02:30:41 +0000

    updating testcase to Vec3D -> Line3D move of splitIntoSegments()
    --HG--
    branch : toxiclibs

--- src.test/toxi/test/geom/Vec3DTest.java
+++ src.test/toxi/test/geom/Vec3DTest.java
@@ -3,6 +3,7 @@ package toxi.test.geom;
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
+import toxi.geom.Line3D;
 import toxi.geom.Vec3D;
 import toxi.math.MathUtils;
 
@@ -22,10 +23,10 @@ public class Vec3DTest extends TestCase {
 	public void testSplitSegments() {
 		Vec3D a = new Vec3D(0, 0, 0);
 		Vec3D b = new Vec3D(100, 0, 0);
-		ArrayList<Vec3D> list = Vec3D.splitIntoSegments(a, b, 8, null, true);
+		ArrayList<Vec3D> list = Line3D.splitIntoSegments(a, b, 8, null, true);
 		assertEquals(14, list.size());
 		// testing adding to existing list and skipping start point
-		Vec3D.splitIntoSegments(b, a, 10, list, false);
+		Line3D.splitIntoSegments(b, a, 10, list, false);
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(i + ": " + list.get(i));
 		}
