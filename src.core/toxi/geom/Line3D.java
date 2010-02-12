62fc43749c746987fb914551ad0c3ada3cc2aedf
Karsten Schmidt <k@postspectacular.com>
2010-02-12 02:55:31 +0000

    eclipse re-formatter run
    --HG--
    branch : toxiclibs

--- src.core/toxi/geom/Line3D.java
+++ src.core/toxi/geom/Line3D.java
@@ -4,81 +4,85 @@ import java.util.ArrayList;
 
 public class Line3D {
 
-	public Vec3D a, b;
+    /**
+     * Splits the line between A and B into segments of the given length,
+     * starting at point A. The tweened points are added to the given result
+     * list. The last point added is B itself and hence it is likely that the
+     * last segment has a shorter length than the step length requested. The
+     * first point (A) can be omitted and not be added to the list if so
+     * desired.
+     * 
+     * @param a
+     *            start point
+     * @param b
+     *            end point (always added to results)
+     * @param stepLength
+     *            desired distance between points
+     * @param segments
+     *            existing array list for results (or a new list, if null)
+     * @param addFirst
+     *            false, if A is NOT to be added to results
+     * @return list of result vectors
+     */
+    public static final ArrayList<Vec3D> splitIntoSegments(Vec3D a, Vec3D b,
+            float stepLength, ArrayList<Vec3D> segments, boolean addFirst) {
+        if (segments == null) {
+            segments = new ArrayList<Vec3D>();
+        }
+        if (addFirst) {
+            segments.add(a.copy());
+        }
+        float dist = a.distanceTo(b);
+        if (dist > stepLength) {
+            Vec3D pos = a.copy();
+            Vec3D step = b.sub(a).limit(stepLength);
+            while (dist > stepLength) {
+                pos.addSelf(step);
+                segments.add(pos.copy());
+                dist -= stepLength;
+            }
+        }
+        segments.add(b.copy());
+        return segments;
+    }
 
-	public Line3D(Vec3D a, Vec3D b) {
-		this.a = a;
-		this.b = b;
-	}
+    public Vec3D a, b;
 
-	public Line3D copy() {
-		return new Line3D(a.copy(), b.copy());
-	}
+    public Line3D(Vec3D a, Vec3D b) {
+        this.a = a;
+        this.b = b;
+    }
 
-	public Vec3D getDirection() {
-		return b.sub(a).normalize();
-	}
+    public Line3D copy() {
+        return new Line3D(a.copy(), b.copy());
+    }
 
-	public float getLength() {
-		return a.distanceTo(b);
-	}
+    public Vec3D getDirection() {
+        return b.sub(a).normalize();
+    }
 
-	public Vec3D getMidPoint() {
-		return a.add(b).scaleSelf(0.5f);
-	}
+    public float getLength() {
+        return a.distanceTo(b);
+    }
 
-	public Vec3D getNormal() {
-		return b.cross(a);
-	}
+    public Vec3D getMidPoint() {
+        return a.add(b).scaleSelf(0.5f);
+    }
 
-	public ArrayList<Vec3D> splitIntoSegments(ArrayList<Vec3D> segments,
-			float stepLength, boolean addFirst) {
-		return splitIntoSegments(a, b, stepLength, segments, addFirst);
-	}
+    public Vec3D getNormal() {
+        return b.cross(a);
+    }
 
-	public Ray3D toRay3D() {
-		return new Ray3D(a.copy(), b.sub(a).normalize());
-	}
+    public ArrayList<Vec3D> splitIntoSegments(ArrayList<Vec3D> segments,
+            float stepLength, boolean addFirst) {
+        return splitIntoSegments(a, b, stepLength, segments, addFirst);
+    }
 
-	/**
-	 * Splits the line between A and B into segments of the given length,
-	 * starting at point A. The tweened points are added to the given result
-	 * list. The last point added is B itself and hence it is likely that the
-	 * last segment has a shorter length than the step length requested. The
-	 * first point (A) can be omitted and not be added to the list if so
-	 * desired.
-	 * 
-	 * @param a
-	 *            start point
-	 * @param b
-	 *            end point (always added to results)
-	 * @param stepLength
-	 *            desired distance between points
-	 * @param segments
-	 *            existing array list for results (or a new list, if null)
-	 * @param addFirst
-	 *            false, if A is NOT to be added to results
-	 * @return list of result vectors
-	 */
-	public static final ArrayList<Vec3D> splitIntoSegments(Vec3D a, Vec3D b,
-			float stepLength, ArrayList<Vec3D> segments, boolean addFirst) {
-		if (segments == null) {
-			segments = new ArrayList<Vec3D>();
-		}
-		if (addFirst) {
-			segments.add(a.copy());
-		}
-		float dist = a.distanceTo(b);
-		if (dist > stepLength) {
-			Vec3D pos = a.copy();
-			Vec3D step = b.sub(a).limit(stepLength);
-			while (dist > stepLength) {
-				pos.addSelf(step);
-				segments.add(pos.copy());
-				dist -= stepLength;
-			}
-		}
-		segments.add(b.copy());
-		return segments;
-	}
+    public Ray3D toRay3D() {
+        return new Ray3D(a.copy(), b.sub(a).normalize());
+    }
+
+    public String toString() {
+        return a.toString() + " -> " + b.toString();
+    }
 }
