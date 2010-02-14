cd9e4c2347828b4646dc3433a6a5641b5786eab7
Karsten Schmidt <k@postspectacular.com>
2010-02-14 12:27:16 +0000

    adding new constructor to create a simple duo-tone ToneMap
    --HG--
    branch : toxiclibs

--- src.color/toxi/color/ToneMap.java
+++ src.color/toxi/color/ToneMap.java
@@ -5,27 +5,36 @@ import toxi.math.ScaleMap;
 
 public class ToneMap {
 
-	public ScaleMap map;
-	public ColorList colors;
+    public ScaleMap map;
+    public ColorList colors;
 
-	public ToneMap(float a, float b, ColorGradient gradient) {
-		this(a, b, gradient.calcGradient());
-	}
+    public ToneMap(float a, float b, ColorGradient gradient) {
+        this(a, b, gradient.calcGradient());
+    }
 
-	public ToneMap(float a, float b, ColorList c) {
-		map = new ScaleMap(a, b, 0, c.size() - 1);
-		colors = c;
-	}
+    public ToneMap(float a, float b, ColorList c) {
+        map = new ScaleMap(a, b, 0, c.size() - 1);
+        colors = c;
+    }
 
-	public int getARGBToneFor(float t) {
-		return colors.get((int) map.getClippedValueFor(t)).toARGB();
-	}
+    public ToneMap(float a, float b, float c, float d, ReadonlyTColor colA,
+            ReadonlyTColor colB) {
+        ColorGradient g = new ColorGradient();
+        g.addColorAt(c, colA);
+        g.addColorAt(d, colB);
+        colors = g.calcGradient();
+        map = new ScaleMap(a, b, 0, colors.size() - 1);
+    }
 
-	public ReadonlyTColor getToneFor(float t) {
-		return colors.get((int) map.getClippedValueFor(t));
-	}
+    public int getARGBToneFor(float t) {
+        return colors.get((int) map.getClippedValueFor(t)).toARGB();
+    }
 
-	public void setMapFunction(InterpolateStrategy func) {
-		map.setMapFunction(func);
-	}
+    public ReadonlyTColor getToneFor(float t) {
+        return colors.get((int) map.getClippedValueFor(t));
+    }
+
+    public void setMapFunction(InterpolateStrategy func) {
+        map.setMapFunction(func);
+    }
 }
