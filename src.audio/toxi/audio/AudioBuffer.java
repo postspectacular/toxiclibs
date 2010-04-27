ea5764395d1ef90a5cc0b2ee4b8d4ae65c78ba05
Karsten Schmidt <k@postspectacular.com>
2010-04-27 14:32:28 +0100

    adding DecompressInputStream and convertUlawToPCM() method to AudioBuffer, enabling support for µ-Law 8bit audio
    --HG--
    branch : toxiclibs

--- src.audio/toxi/audio/AudioBuffer.java
+++ src.audio/toxi/audio/AudioBuffer.java
@@ -20,6 +20,8 @@
 
 package toxi.audio;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import net.java.games.joal.AL;
@@ -33,134 +35,149 @@ import net.java.games.joal.AL;
  */
 public class AudioBuffer {
 
-	/**
-	 * Format descriptor for 8bit mono samples
-	 */
-	public final static int FORMAT_MONO8 = AL.AL_FORMAT_MONO8;
-	/**
-	 * Format descriptor for 16bit mono samples
-	 */
-	public final static int FORMAT_MONO16 = AL.AL_FORMAT_MONO16;
-	/**
-	 * Format descriptor for 8bit stereo samples
-	 */
-	public final static int FORMAT_STEREO8 = AL.AL_FORMAT_STEREO8;
-	/**
-	 * Format descriptor for 16bit stereo samples
-	 */
-	public final static int FORMAT_STEREO16 = AL.AL_FORMAT_STEREO16;
+    /**
+     * Format descriptor for 8bit mono samples
+     */
+    public final static int FORMAT_MONO8 = AL.AL_FORMAT_MONO8;
+    /**
+     * Format descriptor for 16bit mono samples
+     */
+    public final static int FORMAT_MONO16 = AL.AL_FORMAT_MONO16;
+    /**
+     * Format descriptor for 8bit stereo samples
+     */
+    public final static int FORMAT_STEREO8 = AL.AL_FORMAT_STEREO8;
+    /**
+     * Format descriptor for 16bit stereo samples
+     */
+    public final static int FORMAT_STEREO16 = AL.AL_FORMAT_STEREO16;
 
-	protected final AL al;
-	protected ByteBuffer data;
+    protected final AL al;
+    protected ByteBuffer data;
 
-	protected final int id;
+    protected final int id;
 
-	protected final int[] alResult = new int[1];
-	private int format;
+    protected final int[] alResult = new int[1];
+    private int format;
 
-	public AudioBuffer(AL al, int bufferID) {
-		this.id = bufferID;
-		this.al = al;
-	}
+    public AudioBuffer(AL al, int bufferID) {
+        this.id = bufferID;
+        this.al = al;
+    }
 
-	/**
-	 * Configure the audio buffer
-	 * 
-	 * @param data
-	 *            the raw audio data
-	 * @param format
-	 *            the format of the data: <code>FORMAT_MONO8, FORMAT_MONO16,
-	 *        FORMAT_STEREO8</code> and <code>FORMAT_STEREO16</code>
-	 * @param freq
-	 *            the frequency of the data
-	 */
-	public AudioBuffer configure(ByteBuffer data, int format, int freq) {
-		this.data = data;
-		this.format = format;
-		al.alBufferData(id, format, data, data.capacity(), freq);
-		return this;
-	}
+    /**
+     * Configure the audio buffer
+     * 
+     * @param data
+     *            the raw audio data
+     * @param format
+     *            the format of the data: <code>FORMAT_MONO8, FORMAT_MONO16,
+     *        FORMAT_STEREO8</code> and <code>FORMAT_STEREO16</code>
+     * @param freq
+     *            the frequency of the data
+     */
+    public AudioBuffer configure(ByteBuffer data, int format, int freq) {
+        this.data = data;
+        this.format = format;
+        al.alBufferData(id, format, data, data.capacity(), freq);
+        return this;
+    }
 
-	/**
-	 * Deletes this buffer, and frees its resources.
-	 * 
-	 * @return true, if removed successfully
-	 */
-	public boolean delete() {
-		data = null;
-		al.alDeleteBuffers(1, new int[] { id }, 0);
-		return al.alGetError() == AL.AL_NO_ERROR;
-	}
+    public boolean convertUlawToPCM(boolean isAlaw) {
+        byte[] ulaw = new byte[getSampleSize()];
+        data.rewind();
+        data.get(ulaw);
+        byte[] pcm = new byte[ulaw.length];
+        ByteArrayInputStream bin = new ByteArrayInputStream(ulaw);
+        try {
+            new DecompressInputStream(bin, isAlaw).read(pcm);
+            configure(ByteBuffer.wrap(pcm), AudioBuffer.FORMAT_MONO16, 8000);
+            return true;
+        } catch (IOException e) {
+        }
+        return false;
+    }
 
-	/**
-	 * Get the bit-depth of the data, (8 or 16)
-	 * 
-	 * @return the bit-depth of the data
-	 */
-	public final int getBitDepth() {
-		al.alGetBufferi(id, AL.AL_BITS, alResult, 0);
-		return alResult[0];
-	}
+    /**
+     * Deletes this buffer, and frees its resources.
+     * 
+     * @return true, if removed successfully
+     */
+    public boolean delete() {
+        data = null;
+        al.alDeleteBuffers(1, new int[] { id }, 0);
+        return al.alGetError() == AL.AL_NO_ERROR;
+    }
 
-	/**
-	 * Gets the size (in bytes) of the raw data contained in this buffer.
-	 * 
-	 * @return the size of the data.
-	 */
-	public final int getByteSize() {
-		al.alGetBufferi(id, AL.AL_SIZE, alResult, 0);
-		return alResult[0];
-	}
+    /**
+     * Get the bit-depth of the data, (8 or 16)
+     * 
+     * @return the bit-depth of the data
+     */
+    public final int getBitDepth() {
+        al.alGetBufferi(id, AL.AL_BITS, alResult, 0);
+        return alResult[0];
+    }
 
-	/**
-	 * Gets the raw data contained in this buffer.
-	 * 
-	 * @return the raw buffer data.
-	 */
-	public final ByteBuffer getData() {
-		return data;
-	}
+    /**
+     * Gets the size (in bytes) of the raw data contained in this buffer.
+     * 
+     * @return the size of the data.
+     */
+    public final int getByteSize() {
+        al.alGetBufferi(id, AL.AL_SIZE, alResult, 0);
+        return alResult[0];
+    }
 
-	/**
-	 * Gets the audio frequency of the data contained in this buffer.
-	 * 
-	 * @return the frequency of the data
-	 */
-	public final int getFrequency() {
-		al.alGetBufferi(id, AL.AL_FREQUENCY, alResult, 0);
-		return alResult[0];
-	}
+    /**
+     * Gets the raw data contained in this buffer.
+     * 
+     * @return the raw buffer data.
+     */
+    public final ByteBuffer getData() {
+        return data;
+    }
 
-	/**
-	 * Returns the OpenAL reference ID for this buffer.
-	 * 
-	 * @return buffer id
-	 */
-	public final int getID() {
-		return id;
-	}
+    /**
+     * Gets the audio frequency of the data contained in this buffer.
+     * 
+     * @return the frequency of the data
+     */
+    public final int getFrequency() {
+        al.alGetBufferi(id, AL.AL_FREQUENCY, alResult, 0);
+        return alResult[0];
+    }
 
-	/**
-	 * Get the number of channels of the data (1-Mono, 2-Stereo)
-	 * 
-	 * @return the number of audio channels.
-	 */
-	public final int getNumChannels() {
-		al.alGetBufferi(id, AL.AL_CHANNELS, alResult, 0);
+    /**
+     * Returns the OpenAL reference ID for this buffer.
+     * 
+     * @return buffer id
+     */
+    public final int getID() {
+        return id;
+    }
 
-		return alResult[0];
-	}
+    /**
+     * Get the number of channels of the data (1-Mono, 2-Stereo)
+     * 
+     * @return the number of audio channels.
+     */
+    public final int getNumChannels() {
+        al.alGetBufferi(id, AL.AL_CHANNELS, alResult, 0);
 
-	/**
-	 * Gets the size (in samples) of the raw data contained in this buffer.
-	 * 
-	 * @return sample size.
-	 */
-	public final int getSampleSize() {
-		return getByteSize() * 8 / getBitDepth() / getNumChannels();
-	}
+        return alResult[0];
+    }
 
-	public String toString() {
-		return "AudioBuffer: id=" + id + " format=" + format;
-	}
+    /**
+     * Gets the size (in samples) of the raw data contained in this buffer.
+     * 
+     * @return sample size.
+     */
+    public final int getSampleSize() {
+        return getByteSize() * 8 / getBitDepth() / getNumChannels();
+    }
+
+    public String toString() {
+        return "AudioBuffer: id=" + id + " format=" + format;
+    }
 }
