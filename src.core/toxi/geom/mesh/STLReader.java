ae7ca8b977fb3fbf02ec7314f6d2e74e41973d47
Karsten Schmidt <k@postspectacular.com>
2010-07-23 01:50:30 +0100

    major performance update (8x-10x faster) for loadBinary, now using BufferedInputStream and supporting configurable buffer sizes, loadBinary(String) now using FileUtils.createInputStream and supports gzipped files
    --HG--
    branch : toxiclibs

--- src.core/toxi/geom/mesh/STLReader.java
+++ src.core/toxi/geom/mesh/STLReader.java
@@ -1,18 +1,21 @@
 package toxi.geom.mesh;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
 import toxi.geom.Vec3D;
+import toxi.util.FileUtils;
 
 /**
  * Utility class to read binary STL files and turn them into
  * {@link TriangleMesh} instances.
  */
 public class STLReader {
+
+    private static final int DEFAULT_BUFFER_SIZE = 0x8000;
 
     private byte[] buf = new byte[12];
 
@@ -39,9 +42,27 @@ public class STLReader {
      * @return mesh instance or null if unsuccessful
      */
     public TriangleMesh loadBinary(InputStream stream, String meshName) {
+        return loadBinary(stream, meshName, DEFAULT_BUFFER_SIZE);
+    }
+
+    /**
+     * Attempts to load an STL model from the given {@link InputStream}.
+     * Currently no exceptions are being thrown and the method will return null
+     * if anything goes wrong during parsing the mesh data.
+     * 
+     * @param stream
+     * @param meshName
+     * @param bufSize
+     *            size of the stream buffer
+     * @return mesh instance or null if unsuccessful
+     */
+    public TriangleMesh loadBinary(InputStream stream, String meshName,
+            int bufSize) {
         TriangleMesh mesh = null;
         try {
-            DataInputStream ds = new DataInputStream(stream);
+            DataInputStream ds =
+                    new DataInputStream(
+                            new BufferedInputStream(stream, bufSize));
             // read header, ignore color model
             for (int i = 0; i < 80; i++) {
                 ds.read();
@@ -49,14 +70,17 @@ public class STLReader {
             // read num faces
             ds.read(buf, 0, 4);
             int numFaces = bufferToInt();
-            mesh = new TriangleMesh(meshName);
+            mesh = new TriangleMesh(meshName, numFaces, numFaces);
+            Vec3D a = new Vec3D();
+            Vec3D b = new Vec3D();
+            Vec3D c = new Vec3D();
             for (int i = 0; i < numFaces; i++) {
                 // ignore face normal
                 ds.read(buf, 0, 12);
                 // face vertices
-                Vec3D a = readVector(ds);
-                Vec3D b = readVector(ds);
-                Vec3D c = readVector(ds);
+                readVector(ds, a);
+                readVector(ds, b);
+                readVector(ds, c);
                 mesh.addFace(a, c, b);
                 // ignore colour
                 ds.read(buf, 0, 2);
@@ -68,6 +92,10 @@ public class STLReader {
         return mesh;
     }
 
+    public TriangleMesh loadBinary(String fileName) {
+        return loadBinary(fileName, DEFAULT_BUFFER_SIZE);
+    }
+
     /**
      * Attempts to load an STL model from the given file path. Currently no
      * exceptions are being thrown and the method will return null if anything
@@ -77,28 +105,30 @@ public class STLReader {
      *            file path to read model from
      * @return mesh instance or null if unsuccessful
      */
-    public TriangleMesh loadBinary(String fileName) {
+    public TriangleMesh loadBinary(String fileName, int bufSize) {
         TriangleMesh mesh = null;
         try {
             mesh =
-                    loadBinary(new FileInputStream(fileName), fileName
-                            .substring(fileName.lastIndexOf('/') + 1));
-        } catch (FileNotFoundException e) {
+                    loadBinary(FileUtils.createInputStream(new File(fileName)),
+                            fileName.substring(fileName.lastIndexOf('/') + 1),
+                            bufSize);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return mesh;
     }
 
-    private Vec3D readVector(DataInputStream ds) throws IOException {
+    private Vec3D readVector(DataInputStream ds, Vec3D result)
+            throws IOException {
         // x
         ds.read(buf, 0, 4);
-        float x = bufferToFloat();
+        result.x = bufferToFloat();
         // y
         ds.read(buf, 0, 4);
-        float y = bufferToFloat();
+        result.y = bufferToFloat();
         // z
         ds.read(buf, 0, 4);
-        float z = bufferToFloat();
-        return new Vec3D(x, y, z);
+        result.z = bufferToFloat();
+        return result;
     }
 }
