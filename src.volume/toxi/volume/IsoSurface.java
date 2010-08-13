1c75c1e2b1cd82f8f8c1f94c79927d1ae4928c7a
Karsten Schmidt <k@postspectacular.com>
2010-08-13 21:42:22 +0100

    refactored IsoSurface to be much more RAM efficient & faster, removed plain computeSurface() (triangle soup) method (only computeSurfaceMesh() is available)
    --HG--
    branch : wemesh

--- src.volume/toxi/volume/IsoSurface.java
+++ src.volume/toxi/volume/IsoSurface.java
@@ -2,12 +2,9 @@ package toxi.volume;
 
 import java.util.logging.Logger;
 
-import toxi.geom.ReadonlyVec3D;
 import toxi.geom.Vec3D;
-import toxi.geom.mesh.OBJWriter;
-import toxi.geom.mesh.STLWriter;
+import toxi.geom.mesh.Mesh3D;
 import toxi.geom.mesh.TriangleMesh;
-import toxi.util.datatypes.ArrayUtil;
 
 /**
  * IsoSurface class based on C version by Paul Bourke and Lingo version by
@@ -32,12 +29,6 @@ public class IsoSurface {
     protected int nextXY;
 
     protected Vec3D[] edgeVertices;
-    protected int numVertices;
-
-    protected int[] faces;
-    protected int numFaces;
-
-    public ReadonlyVec3D[] faceNormals;
 
     public IsoSurface(VolumetricSpace volume) {
         this.volume = volume;
@@ -58,46 +49,18 @@ public class IsoSurface {
         data = volume.data;
         centreOffset = volume.halfScale.getInverted();
 
-        faces = new int[3 * volume.numCells];
         edgeVertices = new Vec3D[3 * volume.numCells];
-
-        numVertices = 0;
-        numFaces = 0;
-
-        for (int z = 0, index = 0; z < resZ; z++) {
-            for (int y = 0; y < resY; y++) {
-                for (int x = 0; x < resX; x++) {
-                    Vec3D v =
-                            new Vec3D(x * cellSize.x + centreOffset.x, y
-                                    * cellSize.y + centreOffset.y, z
-                                    * cellSize.z + centreOffset.z);
-                    edgeVertices[index] = v;
-                    edgeVertices[index + 1] = v.copy();
-                    edgeVertices[index + 2] = v.copy();
-                    index += 3;
-                }
-            }
-        }
-    }
-
-    public void computeFaceNormals() {
-        if (faceNormals == null || faceNormals.length != numFaces) {
-            faceNormals = new ReadonlyVec3D[numFaces];
-        }
-        Vec3D[] verts = null;
-        for (int i = 0; i < numFaces; i++) {
-            verts = getVerticesForFace(i, verts);
-            ReadonlyVec3D n =
-                    verts[0].sub(verts[1]).crossSelf(verts[0].sub(verts[2]))
-                            .normalize();
-            faceNormals[i] = n;
-        }
     }
 
     /**
      * Computes the surface mesh for the given volumetric data and iso value.
      */
-    public void computeSurface(final float iso) {
+    public Mesh3D computeSurfaceMesh(Mesh3D mesh, final float iso) {
+        if (mesh == null) {
+            mesh = new TriangleMesh("isosurface-" + iso);
+        } else {
+            mesh.clear();
+        }
         isoValue = iso;
         float offsetZ = centreOffset.z;
         for (int z = 0; z < resZ1; z++) {
@@ -108,20 +71,7 @@ public class IsoSurface {
                 int offset = resX * y + sliceOffset;
                 for (int x = 0; x < resX1; x++) {
                     final int cellIndex = getCellIndex(offset);
-                    if (cellIndex > 0) {
-                        int n = 0;
-                        int edgeIndex;
-                        int[] cellTriangles =
-                                MarchingCubesIndex.cellTriangles[cellIndex];
-                        while ((edgeIndex = cellTriangles[n++]) != -1) {
-                            int[] edgeOffsetInfo =
-                                    MarchingCubesIndex.edgeOffsets[edgeIndex];
-                            faces[numVertices++] =
-                                    ((x + edgeOffsetInfo[0]) + resX
-                                            * (y + edgeOffsetInfo[1]) + sliceRes
-                                            * (z + edgeOffsetInfo[2]))
-                                            * 3 + edgeOffsetInfo[3];
-                        }
+                    if (cellIndex > 0 && cellIndex < 255) {
                         final int edgeFlags =
                                 MarchingCubesIndex.edgesToCompute[cellIndex];
                         if (edgeFlags > 0) {
@@ -132,22 +82,30 @@ public class IsoSurface {
                                 float t =
                                         isoDiff
                                                 / (data[offset + 1] - offsetData);
-                                edgeVertices[edgeOffsetIndex].x =
-                                        offsetX + t * cellSize.x;
+                                edgeVertices[edgeOffsetIndex] =
+                                        new Vec3D(offsetX + t * cellSize.x, y
+                                                * cellSize.y + centreOffset.y,
+                                                z * cellSize.z + centreOffset.z);
                             }
                             if ((edgeFlags & 2) > 0) {
                                 float t =
                                         isoDiff
                                                 / (data[offset + resX] - offsetData);
-                                edgeVertices[edgeOffsetIndex + 1].y =
-                                        offsetY + t * cellSize.y;
+                                edgeVertices[edgeOffsetIndex + 1] =
+                                        new Vec3D(x * cellSize.x
+                                                + centreOffset.x, offsetY + t
+                                                * cellSize.y, z * cellSize.z
+                                                + centreOffset.z);
                             }
                             if ((edgeFlags & 4) > 0) {
                                 float t =
                                         isoDiff
                                                 / (data[offset + sliceRes] - offsetData);
-                                edgeVertices[edgeOffsetIndex + 2].z =
-                                        offsetZ + t * cellSize.z;
+                                edgeVertices[edgeOffsetIndex + 2] =
+                                        new Vec3D(x * cellSize.x
+                                                + centreOffset.x, y
+                                                * cellSize.y + centreOffset.y,
+                                                offsetZ + t * cellSize.z);
                             }
                         }
                     }
@@ -158,20 +116,38 @@ public class IsoSurface {
             }
             offsetZ += cellSize.z;
         }
-        numFaces = numVertices / 3;
-    }
 
-    public TriangleMesh computeSurfaceMesh(TriangleMesh mesh, final float iso) {
-        if (mesh == null) {
-            mesh = new TriangleMesh("isosurface-" + iso);
-        } else {
-            mesh.clear();
-        }
-        computeSurface(iso);
-        Vec3D[] verts = null;
-        for (int i = 0; i < numFaces; i++) {
-            verts = getVerticesForFace(i, verts);
-            mesh.addFace(verts[1], verts[2], verts[0]);
+        int[] face = new int[16];
+        for (int z = 0; z < resZ1; z++) {
+            int sliceOffset = sliceRes * z;
+            for (int y = 0; y < resY1; y++) {
+                int offset = resX * y + sliceOffset;
+                for (int x = 0; x < resX1; x++) {
+                    final int cellIndex = getCellIndex(offset);
+                    if (cellIndex > 0 && cellIndex < 255) {
+                        int n = 0;
+                        int edgeIndex;
+                        int[] cellTriangles =
+                                MarchingCubesIndex.cellTriangles[cellIndex];
+                        while ((edgeIndex = cellTriangles[n]) != -1) {
+                            int[] edgeOffsetInfo =
+                                    MarchingCubesIndex.edgeOffsets[edgeIndex];
+                            face[n] =
+                                    ((x + edgeOffsetInfo[0]) + resX
+                                            * (y + edgeOffsetInfo[1]) + sliceRes
+                                            * (z + edgeOffsetInfo[2]))
+                                            * 3 + edgeOffsetInfo[3];
+                            n++;
+                        }
+                        for (int i = 0; i < n; i += 3) {
+                            mesh.addFace(edgeVertices[face[i + 1]],
+                                    edgeVertices[face[i + 2]],
+                                    edgeVertices[face[i]]);
+                        }
+                    }
+                    offset++;
+                }
+            }
         }
         return mesh;
     }
@@ -206,134 +182,13 @@ public class IsoSurface {
         return cellIndex;
     }
 
-    public ReadonlyVec3D getNormalForFace(int faceID) {
-        return faceNormals[faceID];
-    }
-
-    public int getNumFaces() {
-        return numFaces;
-    }
-
-    public int getNumVertices() {
-        return numVertices;
-    }
-
-    /**
-     * Retrieves vertices for the requested mesh face/triangle.
-     * 
-     * @param faceID
-     *            face index
-     * @param vertices
-     *            Vec3D array to place vertices in or null to create a new array
-     *            automatically
-     * @return vertices as Vec3D array
-     */
-    public Vec3D[] getVerticesForFace(int faceID, Vec3D[] vertices) {
-        faceID *= 3;
-        if (vertices == null) {
-            vertices = new Vec3D[3];
-        }
-        vertices[0] = edgeVertices[faces[faceID]];
-        vertices[1] = edgeVertices[faces[faceID + 1]];
-        vertices[2] = edgeVertices[faces[faceID + 2]];
-        return vertices;
-    }
-
     /**
      * Resets mesh vertices to default positions and clears face index. Needs to
      * be called inbetween successive calls to {@link #computeSurface(float)}.
      */
     public void reset() {
-        for (int z = 0, index = 0; z < resZ; z++) {
-            for (int y = 0; y < resY; y++) {
-                for (int x = 0; x < resX && index < numVertices; x++) {
-                    Vec3D v =
-                            edgeVertices[index].set(x * cellSize.x
-                                    + centreOffset.x, y * cellSize.y
-                                    + centreOffset.y, z * cellSize.z
-                                    + centreOffset.z);
-                    edgeVertices[index + 1].set(v);
-                    edgeVertices[index + 2].set(v);
-                    index += 3;
-                }
-            }
-            if (index >= numVertices) {
-                break;
-            }
+        for (int i = 0; i < edgeVertices.length; i++) {
+            edgeVertices[i] = null;
         }
-        numFaces = 0;
-        numVertices = 0;
-    }
-
-    /**
-     * Saves the surface mesh as Wavefront OBJ format and automatically merges
-     * shared vertices.
-     * 
-     * @param fn
-     *            absolute path/filename to write mesh to
-     */
-    public void saveAsOBJ(String fn) {
-        logger.info("saving surface as OBJ to: " + fn);
-        OBJWriter obj = new OBJWriter();
-        obj.beginSave(fn);
-        obj.newObject("isosurface");
-        // figure out unique used vertices
-        int[] faceIndex = new int[numFaces];
-        int numUniqueVertices = 0;
-        for (int i = 0; i < numVertices; i++) {
-            int idx =
-                    ArrayUtil.indexInArray(faces[i], faceIndex,
-                            numUniqueVertices);
-            if (idx == -1) {
-                faceIndex[numUniqueVertices++] = faces[i];
-            }
-            if ((i % 10000) == 0) {
-                logger.fine((i * 100.0 / numFaces) + "% unique:"
-                        + numUniqueVertices);
-            }
-        }
-        for (int i = 0; i < numUniqueVertices; i++) {
-            obj.vertex(edgeVertices[faceIndex[i]]);
-        }
-        logger.info("creating facelist...");
-        obj.faceList();
-        for (int i = 0; i < numVertices; i += 3) {
-            int a =
-                    ArrayUtil.indexInArray(faces[i], faceIndex,
-                            numUniqueVertices);
-            int b =
-                    ArrayUtil.indexInArray(faces[i + 1], faceIndex,
-                            numUniqueVertices);
-            int c =
-                    ArrayUtil.indexInArray(faces[i + 2], faceIndex,
-                            numUniqueVertices);
-            obj.face(c + 1, b + 1, a + 1);
-            if ((i % 10000) == 0) {
-                logger.fine((i * 100.0 / numFaces) + "%");
-            }
-        }
-        obj.endSave();
-    }
-
-    /**
-     * Saves surface mesh as binary STL file.
-     * 
-     * @param fn
-     *            absolute path/filename to save to
-     */
-    public void saveAsSTL(String fn) {
-        logger.info("saving surface as STL to: " + fn);
-        STLWriter stl = new STLWriter();
-        stl.beginSave(fn, numFaces);
-        for (int i = 0, off = 0; i < numFaces; i++) {
-            Vec3D a = edgeVertices[faces[off++]];
-            Vec3D b = edgeVertices[faces[off++]];
-            Vec3D c = edgeVertices[faces[off++]];
-            stl.face(c, b, a);
-            if ((i % 10000) == 0) {
-                logger.fine((i * 100.0 / numFaces) + "%");
-            }
-        }
-        stl.endSave();
     }
 }
