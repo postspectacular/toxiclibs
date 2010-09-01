6d963edeffb0a83b13798c4c16cb12061a3c51dd
Karsten Schmidt <k@postspectacular.com>
2010-09-01 17:44:45 +0100

    adding ultrabasic version of solidifyVolume() to MeshVoxelizer class. This doesn't currently work with any kind of convexness in the volume space and further voxel-mesh collision tests are needed to figure out if a voxel is within the model or not (check normal of triangle intersecting a voxel)
    --HG--
    branch : wemesh

--- src.volume/toxi/volume/MeshVoxelizer.java
+++ src.volume/toxi/volume/MeshVoxelizer.java
@@ -6,15 +6,40 @@ import toxi.geom.Triangle;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.Face;
 import toxi.geom.mesh.Mesh3D;
+import toxi.geom.mesh.WETriangleMesh;
 
 public class MeshVoxelizer {
+
+    public static VolumetricSpace solidifyVolume(VolumetricSpaceArray volume) {
+        for (int z = 0; z < volume.resZ; z++) {
+            for (int y = 0; y < volume.resY; y++) {
+                boolean isFilled = false;
+                int startX = 0;
+                for (int x = 0; x < volume.resX; x++) {
+                    float val = volume.getVoxelAt(x, y, z);
+                    if (val > 0) {
+                        if (!isFilled) {
+                            startX = x;
+                            isFilled = true;
+                        } else {
+                            for (int i = startX; i <= x; i++) {
+                                volume.setVoxelAt(i, y, z, 1);
+                            }
+                            isFilled = false;
+                        }
+                    }
+                }
+            }
+        }
+        return volume;
+    }
 
     public static VolumetricSpace voxelizeMesh(Mesh3D mesh, int resX, int resY,
             int resZ) {
         mesh.center(null);
         AABB box = mesh.getBoundingBox();
         Vec3D scale = box.getExtent();
-        AABB volBox = new AABB(box, scale.scale(1.5f));
+        AABB volBox = new AABB(box, scale.scale(2f));
         VolumetricSpaceArray volume =
                 new VolumetricSpaceArray(volBox.getExtent().scale(2), resX,
                         resY, resZ);
@@ -47,5 +72,9 @@ public class MeshVoxelizer {
             }
         }
         return volume;
+    }
+
+    public static VolumetricSpace voxelizeMesh(WETriangleMesh mesh, int res) {
+        return voxelizeMesh(mesh, res, res, res);
     }
 }
