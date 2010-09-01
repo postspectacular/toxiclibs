2e52195e55a3a2579e277154efd018a395e14457
Karsten Schmidt <k@postspectacular.com>
2010-09-01 12:15:35 +0100

    refactored/optimized MeshVoxelizer to iterate over mesh faces only once in the outer loop and only check voxels within the bounding box of each triangle
    --HG--
    branch : wemesh

--- src.volume/toxi/volume/MeshVoxelizer.java
+++ src.volume/toxi/volume/MeshVoxelizer.java
@@ -1,6 +1,7 @@
 package toxi.volume;
 
 import toxi.geom.AABB;
+import toxi.geom.Matrix4x4;
 import toxi.geom.Triangle;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.Face;
@@ -10,28 +11,36 @@ public class MeshVoxelizer {
 
     public static VolumetricSpace voxelizeMesh(Mesh3D mesh, int resX, int resY,
             int resZ) {
-        Vec3D scale = mesh.getBoundingBox().getExtent();
-        Vec3D volScale =
-                scale.scale(2 + 2f / resX, 2 + 2f / resY, 2 + 2f / resZ);
+        mesh.center(null);
+        AABB box = mesh.getBoundingBox();
+        Vec3D scale = box.getExtent();
+        AABB volBox = new AABB(box, scale.scale(1.5f));
         VolumetricSpaceArray volume =
-                new VolumetricSpaceArray(volScale, resX + 2, resY + 2, resZ + 2);
+                new VolumetricSpaceArray(volBox.getExtent().scale(2), resX,
+                        resY, resZ);
         Triangle tri = new Triangle();
-        AABB voxel =
-                new AABB(new Vec3D(), scale.scale(1f / resX, 1f / resY,
-                        1f / resZ));
-        for (int z = 1; z < volume.resZ1; z++) {
-            for (int y = 1; y < volume.resY1; y++) {
-                for (int x = 1; x < volume.resX1; x++) {
-                    Vec3D pos =
-                            new Vec3D(x - 1, y - 1, z - 1).scaleSelf(
-                                    volume.voxelSize).subSelf(volume.halfScale);
-                    voxel.set(pos);
-                    for (Face f : mesh.getFaces()) {
-                        tri.a = f.a;
-                        tri.b = f.b;
-                        tri.c = f.c;
-                        if (voxel.intersectsTriangle(tri)) {
-                            volume.setVoxelAt(x, y, z, 1);
+        AABB voxel = new AABB(new Vec3D(), volume.voxelSize.scale(0.5f));
+        Matrix4x4 matWorld2Grid =
+                new Matrix4x4().scaleSelf(volume.voxelSize.getReciprocal())
+                        .translateSelf(volBox.getMin().getInverted());
+        Matrix4x4 matGrid2World = matWorld2Grid.getInverted();
+        for (Face f : mesh.getFaces()) {
+            tri.a = f.a;
+            tri.b = f.b;
+            tri.c = f.c;
+            AABB bounds = tri.getBoundingBox();
+            Vec3D min = matWorld2Grid.applyTo(bounds.getMin());
+            Vec3D max = matWorld2Grid.applyTo(bounds.getMax());
+            for (int z = (int) min.z; z <= max.z; z++) {
+                for (int y = (int) min.y; y <= max.y; y++) {
+                    for (int x = (int) min.x; x <= max.x; x++) {
+                        if (x < volume.resX && y < volume.resY
+                                && z < volume.resZ) {
+                            voxel.set(matGrid2World.applyToSelf(new Vec3D(x, y,
+                                    z)));
+                            if (voxel.intersectsTriangle(tri)) {
+                                volume.setVoxelAt(x, y, z, 1);
+                            }
                         }
                     }
                 }
