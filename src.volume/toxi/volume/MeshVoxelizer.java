c1a976d67f7b24d3f3d52a2cb8e09cccc35ad630
Karsten Schmidt <k@postspectacular.com>
2010-09-05 01:28:29 +0100

    resolving scaling issues/clipping in MeshVoxelizer
    --HG--
    branch : wemesh

--- src.volume/toxi/volume/MeshVoxelizer.java
+++ src.volume/toxi/volume/MeshVoxelizer.java
@@ -1,12 +1,12 @@
 package toxi.volume;
 
 import toxi.geom.AABB;
-import toxi.geom.Matrix4x4;
 import toxi.geom.Triangle;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.Face;
 import toxi.geom.mesh.Mesh3D;
 import toxi.geom.mesh.WETriangleMesh;
+import toxi.math.ScaleMap;
 
 public class MeshVoxelizer {
 
@@ -36,33 +36,43 @@ public class MeshVoxelizer {
 
     public static VolumetricSpace voxelizeMesh(Mesh3D mesh, int resX, int resY,
             int resZ) {
-        mesh.center(null);
         AABB box = mesh.getBoundingBox();
-        Vec3D scale = box.getExtent();
-        AABB volBox = new AABB(box, scale.scale(2f));
-        VolumetricSpaceArray volume =
-                new VolumetricSpaceArray(volBox.getExtent().scale(2), resX,
-                        resY, resZ);
+        Vec3D bmin = box.getMin();
+        Vec3D bmax = box.getMax();
+        ScaleMap wx = new ScaleMap(bmin.x, bmax.x, 1, resX - 2);
+        ScaleMap wy = new ScaleMap(bmin.y, bmax.y, 1, resY - 2);
+        ScaleMap wz = new ScaleMap(bmin.z, bmax.z, 1, resZ - 2);
+        ScaleMap gx = new ScaleMap(1, resX - 2, bmin.x, bmax.x);
+        ScaleMap gy = new ScaleMap(1, resY - 2, bmin.y, bmax.y);
+        ScaleMap gz = new ScaleMap(1, resZ - 2, bmin.z, bmax.z);
+        VolumetricSpace volume =
+                new VolumetricHashMap(box.getExtent().scale(2f), resX, resY,
+                        resZ, 0.1f);
         Triangle tri = new Triangle();
         AABB voxel = new AABB(new Vec3D(), volume.voxelSize.scale(0.5f));
-        Matrix4x4 matWorld2Grid =
-                new Matrix4x4().scaleSelf(volume.voxelSize.getReciprocal())
-                        .translateSelf(volBox.getMin().getInverted());
-        Matrix4x4 matGrid2World = matWorld2Grid.getInverted();
         for (Face f : mesh.getFaces()) {
             tri.a = f.a;
             tri.b = f.b;
             tri.c = f.c;
             AABB bounds = tri.getBoundingBox();
-            Vec3D min = matWorld2Grid.applyTo(bounds.getMin());
-            Vec3D max = matWorld2Grid.applyTo(bounds.getMax());
+            Vec3D min = bounds.getMin();
+            Vec3D max = bounds.getMax();
+            min =
+                    new Vec3D((int) wx.getClippedValueFor(min.x), (int) wy
+                            .getClippedValueFor(min.y), (int) wz
+                            .getClippedValueFor(min.z));
+            max =
+                    new Vec3D((int) wx.getClippedValueFor(max.x), (int) wy
+                            .getClippedValueFor(max.y), (int) wz
+                            .getClippedValueFor(max.z));
             for (int z = (int) min.z; z <= max.z; z++) {
                 for (int y = (int) min.y; y <= max.y; y++) {
                     for (int x = (int) min.x; x <= max.x; x++) {
-                        if (x < volume.resX && y < volume.resY
-                                && z < volume.resZ) {
-                            voxel.set(matGrid2World.applyToSelf(new Vec3D(x, y,
-                                    z)));
+                        if (x < volume.resX1 && y < volume.resY1
+                                && z < volume.resZ1) {
+                            voxel.set((float) gx.getClippedValueFor(x),
+                                    (float) gy.getClippedValueFor(y),
+                                    (float) gz.getClippedValueFor(z));
                             if (voxel.intersectsTriangle(tri)) {
                                 volume.setVoxelAt(x, y, z, 1);
                             }
