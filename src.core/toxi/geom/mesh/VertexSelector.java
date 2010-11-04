d6e87f51f2e3f8731e0c509fe124d908e579ddb0
Karsten Schmidt <k@postspectacular.com>
2010-11-04 01:05:45 +0000

    refactoring VertexSelector into abstract class with new HashSet based selection collection and boolean set operations to merge multiple selections, refactoring PlaneSelector to work with any Plane instance and not be limited to major axis aligned planes and added classification and tolerance param, added DefaultSelector implementation which selects entire mesh
    --HG--
    branch : toxiclibs

--- src.core/toxi/geom/mesh/VertexSelector.java
+++ src.core/toxi/geom/mesh/VertexSelector.java
@@ -1,8 +1,63 @@
 package toxi.geom.mesh;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
-public interface VertexSelector {
+import toxi.geom.Vec3D;
 
-    public List<Vertex> selectVertices(Mesh3D mesh, List<Vertex> selection);
+public abstract class VertexSelector {
+
+    protected Mesh3D mesh;
+    protected Set<Vertex> selection;
+
+    public VertexSelector(Mesh3D mesh) {
+        this.mesh = mesh;
+    }
+
+    public Collection<Vertex> addSelection(Collection<Vertex> sel2) {
+        selection.addAll(sel2);
+        return selection;
+    }
+
+    public VertexSelector clearSelection() {
+        selection.clear();
+        return this;
+    }
+
+    public Collection<Vertex> getSelection() {
+        if (selection == null) {
+            selection = new HashSet<Vertex>();
+        }
+        return selection;
+    }
+
+    public Collection<Vertex> invertSelection() {
+        HashSet<Vertex> newSel =
+                new HashSet<Vertex>(mesh.getNumVertices() - selection.size());
+        for (Vertex v : mesh.getVertices()) {
+            if (!selection.contains(v)) {
+                newSel.add(v);
+            }
+        }
+        selection = newSel;
+        return selection;
+    }
+
+    public Collection<Vertex> selectSimilar(Collection<? extends Vec3D> sel2) {
+        if (selection == null) {
+            selection = new HashSet<Vertex>(sel2.size());
+        }
+        for (Vec3D v : sel2) {
+            selection.add(mesh.getClosestVertexToPoint(v));
+        }
+        return selection;
+    }
+
+    public abstract Collection<Vertex> selectVertices();
+
+    public Collection<Vertex> subtractSelection(Collection<Vertex> sel2) {
+        selection.removeAll(sel2);
+        return selection;
+    }
 }
