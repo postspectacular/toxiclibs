eec7a4956c46091aea40c163fa53623f5e6f74e3
Karsten Schmidt <k@postspectacular.com>
2011-01-10 15:53:23 +0000

    importing & refactoring matrix classes from javax.vecmath package: GMatrix, GVector, Matrix3d, Matrix4f + exceptions
    --HG--
    branch : toxiclibs

---
+++ src.core/toxi/geom/InterpolationException.java
@@ -0,0 +1,49 @@
+/*
+ * jgeom: Geometry Library fo Java
+ * 
+ * Copyright (C) 2005  Samuel Gerber
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package toxi.geom;
+
+/**
+ * An InterpolationException is thrown if Nurbs could not be interpolated from
+ * the given points.
+ * 
+ * @author sg
+ * @version 1.0
+ */
+public class InterpolationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public InterpolationException() {
+        super();
+    }
+
+    public InterpolationException(String arg0) {
+        super(arg0);
+    }
+
+    public InterpolationException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+
+    public InterpolationException(Throwable arg0) {
+        super(arg0);
+    }
+
+}
---
+++ src.core/toxi/geom/MatrixSizeException.java
@@ -0,0 +1,59 @@
+/*
+ * $RCSfile$
+ *
+ * Copyright 1997-2008 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ * $Revision: 127 $
+ * $Date: 2008-02-28 20:18:51 +0000 (Thu, 28 Feb 2008) $
+ * $State$
+ */
+
+package toxi.geom;
+
+/**
+ * Indicates that an operation cannot be completed properly because of a
+ * mismatch in the sizes of object attributes.
+ */
+public class MatrixSizeException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create the exception object with default values.
+     */
+    public MatrixSizeException() {
+    }
+
+    /**
+     * Create the exception object that outputs a message.
+     * 
+     * @param str
+     *            the message string to be output.
+     */
+    public MatrixSizeException(String str) {
+
+        super(str);
+    }
+
+}
