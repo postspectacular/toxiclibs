f9ffcb5a7edcb1adafd922f764a829267859793c
Karsten Schmidt <k@postspectacular.com>
2011-01-10 15:32:09 +0000

    extracting MeshLatticeBuilder.buildVolume() from build() method, fixing vertex coordinate to voxel mapping
    --HG--
    branch : toxiclibs

--- src.volume/toxi/volume/MeshLatticeBuilder.java
+++ src.volume/toxi/volume/MeshLatticeBuilder.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import toxi.geom.AABB;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.WETriangleMesh;
 import toxi.geom.mesh.WingedEdge;
@@ -42,27 +43,7 @@ public class MeshLatticeBuilder {
 
     public static WETriangleMesh build(WETriangleMesh mesh, int res,
             FloatRange stroke) {
-        logger.info("creating lattice...");
-        VolumetricHashMap volume =
-                new VolumetricHashMap(mesh.getBoundingBox().getExtent()
-                        .scale(2), res, res, res, 0.33f);
-        VolumetricBrush brush = new RoundBrush(volume, 1);
-        List<Float> edgeLengths = new ArrayList<Float>(mesh.edges.size());
-        for (WingedEdge e : mesh.edges.values()) {
-            edgeLengths.add(e.getLength());
-        }
-        FloatRange range = FloatRange.fromSamples(edgeLengths);
-        ScaleMap brushSize =
-                new ScaleMap(range.min, range.max, stroke.min, stroke.max);
-        for (WingedEdge e : mesh.edges.values()) {
-            List<Vec3D> points = e.splitIntoSegments(null, 1, true);
-            brush.setSize((float) brushSize.getClippedValueFor(e.getLength()));
-            for (Vec3D p : points) {
-                brush.drawAtAbsolutePos(p, 0.5f);
-            }
-        }
-        logger.info("volume density: " + volume.getDensity());
-        volume.closeSides();
+        VolumetricSpace volume = buildVolume(mesh, res, stroke);
         mesh = null;
         IsoSurface surface;
         surface = new HashIsoSurface(volume);
@@ -75,4 +56,43 @@ public class MeshLatticeBuilder {
         return mesh;
     }
 
+    public static VolumetricSpace buildVolume(WETriangleMesh mesh, int res,
+            float stroke) {
+        return buildVolume(mesh, res, new FloatRange(stroke, stroke));
+    }
+
+    public static VolumetricSpace buildVolume(WETriangleMesh mesh, int res,
+            FloatRange stroke) {
+        logger.info("creating lattice...");
+        AABB box = mesh.getBoundingBox();
+        Vec3D bmin = box.getMin();
+        Vec3D bmax = box.getMax();
+        ScaleMap wx = new ScaleMap(bmin.x, bmax.x, 1, res - 2);
+        ScaleMap wy = new ScaleMap(bmin.y, bmax.y, 1, res - 2);
+        ScaleMap wz = new ScaleMap(bmin.z, bmax.z, 1, res - 2);
+        VolumetricHashMap volume =
+                new VolumetricHashMap(box.getExtent().scale(2), res, res, res,
+                        0.33f);
+        VolumetricBrush brush = new RoundBrush(volume, 1);
+        List<Float> edgeLengths = new ArrayList<Float>(mesh.edges.size());
+        for (WingedEdge e : mesh.edges.values()) {
+            edgeLengths.add(e.getLength());
+        }
+        FloatRange range = FloatRange.fromSamples(edgeLengths);
+        ScaleMap brushSize =
+                new ScaleMap(range.min, range.max, stroke.min, stroke.max);
+        for (WingedEdge e : mesh.edges.values()) {
+            List<Vec3D> points = e.splitIntoSegments(null, 0.5f, true);
+            brush.setSize((float) brushSize.getClippedValueFor(e.getLength()));
+            for (Vec3D p : points) {
+                float x = (float) wx.getClippedValueFor(p.x);
+                float y = (float) wy.getClippedValueFor(p.y);
+                float z = (float) wz.getClippedValueFor(p.z);
+                brush.drawAtGridPos(x, y, z, 1);
+            }
+        }
+        logger.info("volume density: " + volume.getDensity());
+        volume.closeSides();
+        return volume;
+    }
 }
