b90ea25a7d143d5cc5dc9ba23ba825df7413b8f1
postspectacular <none@none>
2011-03-24 12:10:03 +0000

    adding FileUtils.showDirectoryChooser(), adding new version for showFileDialog() accepting a FileNameFilter, adding LOAD/SAVE constants for FileDialogs to avoid extra AWT import statements
    --HG--
    branch : toxiclibs

--- src.core/toxi/util/FileUtils.java
+++ src.core/toxi/util/FileUtils.java
@@ -51,204 +51,266 @@ import java.util.zip.GZIPOutputStream;
  */
 public class FileUtils {
 
-    static public void createDirectories(File file) {
-        try {
-            String parentName = file.getParent();
-            if (parentName != null) {
-                File parent = new File(parentName);
-                if (!parent.exists()) {
-                    parent.mkdirs();
-                }
-            }
-        } catch (SecurityException se) {
-            System.err.println("No permissions to create "
-                    + file.getAbsolutePath());
-        }
-    }
+	public static final int LOAD = FileDialog.LOAD;
+	public static final int SAVE = FileDialog.SAVE;
 
-    static public InputStream createInputStream(File file) throws IOException {
-        if (file == null) {
-            throw new IllegalArgumentException("file can't be null");
-        }
-        InputStream stream = new FileInputStream(file);
-        if (file.getName().toLowerCase().endsWith(".gz")) {
-            stream = new GZIPInputStream(stream);
-        }
-        return stream;
-    }
+	static public void createDirectories(File file) {
+		try {
+			String parentName = file.getParent();
+			if (parentName != null) {
+				File parent = new File(parentName);
+				if (!parent.exists()) {
+					parent.mkdirs();
+				}
+			}
+		} catch (SecurityException se) {
+			System.err.println("No permissions to create "
+					+ file.getAbsolutePath());
+		}
+	}
 
-    static public OutputStream createOutputStream(File file) throws IOException {
-        if (file == null) {
-            throw new IllegalArgumentException("file can't be null");
-        }
-        createDirectories(file);
-        OutputStream stream = new FileOutputStream(file);
-        if (file.getName().toLowerCase().endsWith(".gz")) {
-            stream = new GZIPOutputStream(stream);
-        }
-        return stream;
-    }
+	static public InputStream createInputStream(File file) throws IOException {
+		if (file == null) {
+			throw new IllegalArgumentException("file can't be null");
+		}
+		InputStream stream = new FileInputStream(file);
+		if (file.getName().toLowerCase().endsWith(".gz")) {
+			stream = new GZIPInputStream(stream);
+		}
+		return stream;
+	}
 
-    public static BufferedReader createReader(File file) throws IOException {
-        return createReader(createInputStream(file));
-    }
+	static public OutputStream createOutputStream(File file) throws IOException {
+		if (file == null) {
+			throw new IllegalArgumentException("file can't be null");
+		}
+		createDirectories(file);
+		OutputStream stream = new FileOutputStream(file);
+		if (file.getName().toLowerCase().endsWith(".gz")) {
+			stream = new GZIPOutputStream(stream);
+		}
+		return stream;
+	}
 
-    public static BufferedReader createReader(InputStream input) {
-        return createReader(input, "UTF-8");
-    }
+	public static BufferedReader createReader(File file) throws IOException {
+		return createReader(createInputStream(file));
+	}
 
-    public static BufferedReader createReader(InputStream input, String encoding) {
-        InputStreamReader isr = null;
-        try {
-            isr = new InputStreamReader(input, encoding);
-        } catch (UnsupportedEncodingException e) {
-        }
-        return new BufferedReader(isr);
-    }
+	public static BufferedReader createReader(InputStream input) {
+		return createReader(input, "UTF-8");
+	}
 
-    public static PrintWriter createWriter(File file) throws IOException {
-        return createWriter(createOutputStream(file));
-    }
+	public static BufferedReader createReader(InputStream input, String encoding) {
+		InputStreamReader isr = null;
+		try {
+			isr = new InputStreamReader(input, encoding);
+		} catch (UnsupportedEncodingException e) {
+		}
+		return new BufferedReader(isr);
+	}
 
-    public static PrintWriter createWriter(OutputStream out) {
-        return createWriter(out, "UTF-8");
-    }
+	public static PrintWriter createWriter(File file) throws IOException {
+		return createWriter(createOutputStream(file));
+	}
 
-    public static PrintWriter createWriter(OutputStream out, String encoding) {
-        OutputStreamWriter w = null;
-        try {
-            w = new OutputStreamWriter(out, encoding);
-        } catch (UnsupportedEncodingException e) {
-        }
-        return new PrintWriter(w);
-    }
+	public static PrintWriter createWriter(OutputStream out) {
+		return createWriter(out, "UTF-8");
+	}
 
-    /**
-     * <p>
-     * Analyses the given file path for a file sequence pattern and returns a
-     * {@link FileSequenceDescriptor} instance for further use to handle this
-     * sequence. The file pattern should be in one of these formats:
-     * </p>
-     * <ul>
-     * <li>base_path-00001.ext</li>
-     * <li>base_path001.ext</li>
-     * </ul>
-     * <p>
-     * The sequence index should be using leading zeros, but the number of
-     * digits will be identified automatically.
-     * </p>
-     * 
-     * @param path
-     *            file path of the first file in the sequence
-     * @return descriptor, or null, if the path could not be analysed
-     */
-    public static FileSequenceDescriptor getFileSequenceDescriptorFor(
-            String path) {
-        int dotIndex = path.lastIndexOf('.');
-        int zeroIndex = path.lastIndexOf('-') + 1;
-        if (zeroIndex == 0) {
-            zeroIndex = dotIndex - 1;
-            while (path.charAt(zeroIndex) >= '0'
-                    && path.charAt(zeroIndex) <= '9') {
-                zeroIndex--;
-            }
-            zeroIndex++;
-        }
-        int numDigits = dotIndex - zeroIndex;
-        if (dotIndex != -1 && numDigits > 0) {
-            String base = path.substring(0, zeroIndex);
-            String extension = path.substring(dotIndex);
-            String filePattern = base + "%0" + numDigits + "d" + extension;
-            int start = Integer.parseInt(path.substring(zeroIndex, dotIndex));
-            return new FileSequenceDescriptor(filePattern, extension, dotIndex
-                    - zeroIndex, start);
-        } else {
-            return null;
-        }
-    }
+	public static PrintWriter createWriter(OutputStream out, String encoding) {
+		OutputStreamWriter w = null;
+		try {
+			w = new OutputStreamWriter(out, encoding);
+		} catch (UnsupportedEncodingException e) {
+		}
+		return new PrintWriter(w);
+	}
 
-    static public byte[] loadBytes(InputStream stream) throws IOException {
-        BufferedInputStream input = new BufferedInputStream(stream);
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        int c;
-        while ((c = input.read()) != -1) {
-            buffer.write(c);
-        }
-        return buffer.toByteArray();
-    }
+	/**
+	 * <p>
+	 * Analyses the given file path for a file sequence pattern and returns a
+	 * {@link FileSequenceDescriptor} instance for further use to handle this
+	 * sequence. The file pattern should be in one of these formats:
+	 * </p>
+	 * <ul>
+	 * <li>base_path-00001.ext</li>
+	 * <li>base_path001.ext</li>
+	 * </ul>
+	 * <p>
+	 * The sequence index should be using leading zeros, but the number of
+	 * digits will be identified automatically.
+	 * </p>
+	 * 
+	 * @param path
+	 *            file path of the first file in the sequence
+	 * @return descriptor, or null, if the path could not be analysed
+	 */
+	public static FileSequenceDescriptor getFileSequenceDescriptorFor(
+			String path) {
+		int dotIndex = path.lastIndexOf('.');
+		int zeroIndex = path.lastIndexOf('-') + 1;
+		if (zeroIndex == 0) {
+			zeroIndex = dotIndex - 1;
+			while (path.charAt(zeroIndex) >= '0'
+					&& path.charAt(zeroIndex) <= '9') {
+				zeroIndex--;
+			}
+			zeroIndex++;
+		}
+		int numDigits = dotIndex - zeroIndex;
+		if (dotIndex != -1 && numDigits > 0) {
+			String base = path.substring(0, zeroIndex);
+			String extension = path.substring(dotIndex);
+			String filePattern = base + "%0" + numDigits + "d" + extension;
+			int start = Integer.parseInt(path.substring(zeroIndex, dotIndex));
+			return new FileSequenceDescriptor(filePattern, extension, dotIndex
+					- zeroIndex, start);
+		} else {
+			return null;
+		}
+	}
 
-    public static String loadText(BufferedReader r) throws IOException {
-        StringBuilder result = new StringBuilder();
-        String line;
-        while ((line = r.readLine()) != null) {
-            result.append(line).append("\n");
-        }
-        return result.toString();
-    }
+	static public byte[] loadBytes(InputStream stream) throws IOException {
+		BufferedInputStream input = new BufferedInputStream(stream);
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int c;
+		while ((c = input.read()) != -1) {
+			buffer.write(c);
+		}
+		return buffer.toByteArray();
+	}
 
-    public static String loadText(InputStream input) throws IOException {
-        return loadText(input, "UTF-8");
-    }
+	public static String loadText(BufferedReader r) throws IOException {
+		StringBuilder result = new StringBuilder();
+		String line;
+		while ((line = r.readLine()) != null) {
+			result.append(line).append("\n");
+		}
+		return result.toString();
+	}
 
-    public static String loadText(InputStream input, String encoding)
-            throws IOException {
-        byte[] raw = loadBytes(input);
-        return new String(raw, encoding);
-    }
+	public static String loadText(InputStream input) throws IOException {
+		return loadText(input, "UTF-8");
+	}
 
-    static public void saveText(OutputStream output, String string) {
-        saveText(createWriter(output), string);
-    }
+	public static String loadText(InputStream input, String encoding)
+			throws IOException {
+		byte[] raw = loadBytes(input);
+		return new String(raw, encoding);
+	}
 
-    static public void saveText(PrintWriter writer, String string) {
-        writer.println(string);
-        writer.flush();
-        writer.close();
-    }
+	static public void saveText(OutputStream output, String string) {
+		saveText(createWriter(output), string);
+	}
 
-    /**
-     * Displays a standard AWT file dialog for choosing a file for loading or
-     * saving.
-     * 
-     * @param frame
-     *            parent frame
-     * @param title
-     *            dialog title
-     * @param path
-     *            base directory (or null)
-     * @param formats
-     *            an array of allowed file extensions (or null to allow all)
-     * @param mode
-     *            either FileDialog.LOAD or FileDialog.SAVE
-     * @return path to chosen file or null, if user has cancelled
-     */
-    public static String showFileDialog(final Frame frame, final String title,
-            String path, final String[] formats, final int mode) {
-        String fileID = null;
-        FileDialog fd = new FileDialog(frame, title, mode);
-        if (path != null) {
-            fd.setDirectory(path);
-        }
-        if (formats != null) {
-            fd.setFilenameFilter(new FilenameFilter() {
+	static public void saveText(PrintWriter writer, String string) {
+		writer.println(string);
+		writer.flush();
+		writer.close();
+	}
 
-                public boolean accept(File dir, String name) {
-                    boolean isAccepted = false;
-                    for (String ext : formats) {
-                        if (name.indexOf(ext) != -1) {
-                            isAccepted = true;
-                            break;
-                        }
-                    }
-                    return isAccepted;
-                }
-            });
-        }
-        fd.setVisible(true);
-        if (fd.getFile() != null) {
-            fileID = fd.getFile();
-            fileID = fd.getDirectory() + fileID;
-        }
-        return fileID;
-    }
+	/**
+	 * Displays a standard AWT file dialog for choosing a folder (no files!).
+	 * 
+	 * @param frame
+	 *            parent frame
+	 * @param title
+	 *            dialog title
+	 * @param path
+	 *            base directory (or null)
+	 * @return path to chosen directory or null, if user has canceled
+	 */
+	public static String showDirectoryChooser(final Frame frame,
+			final String title, String path) {
+		System.setProperty("apple.awt.fileDialogForDirectories", "true");
+		String result = showFileDialog(frame, title, path,
+				new FilenameFilter() {
+
+					public boolean accept(File dir, String name) {
+						return new File(dir + "/" + name).isDirectory();
+					}
+				}, LOAD);
+		System.setProperty("apple.awt.fileDialogForDirectories", "false");
+		return result;
+	}
+
+	/**
+	 * Displays a standard AWT file dialog for choosing a file for loading or
+	 * saving.
+	 * 
+	 * @param frame
+	 *            parent frame
+	 * @param title
+	 *            dialog title
+	 * @param path
+	 *            base directory (or null)
+	 * @param filter
+	 *            a FilenameFilter implementation (or null)
+	 * @param mode
+	 *            either FileUtils.LOAD or FileUtils.SAVE
+	 * @return path to chosen file or null, if user has canceled
+	 */
+	public static String showFileDialog(final Frame frame, final String title,
+			String path, FilenameFilter filter, final int mode) {
+		String fileID = null;
+		FileDialog fd = new FileDialog(frame, title, mode);
+		if (path != null) {
+			fd.setDirectory(path);
+		}
+		if (filter != null) {
+			fd.setFilenameFilter(filter);
+		}
+		fd.setVisible(true);
+		if (fd.getFile() != null) {
+			fileID = fd.getFile();
+			fileID = fd.getDirectory() + fileID;
+		}
+		return fileID;
+	}
+
+	/**
+	 * Displays a standard AWT file dialog for choosing a file for loading or
+	 * saving.
+	 * 
+	 * @param frame
+	 *            parent frame
+	 * @param title
+	 *            dialog title
+	 * @param path
+	 *            base directory (or null)
+	 * @param formats
+	 *            an array of allowed file extensions (or null to allow all)
+	 * @param mode
+	 *            either FileUtils.LOAD or FileUtils.SAVE
+	 * @return path to chosen file or null, if user has canceled
+	 */
+	public static String showFileDialog(final Frame frame, final String title,
+			String path, final String[] formats, final int mode) {
+		String fileID = null;
+		FileDialog fd = new FileDialog(frame, title, mode);
+		if (path != null) {
+			fd.setDirectory(path);
+		}
+		if (formats != null) {
+			fd.setFilenameFilter(new FilenameFilter() {
+
+				public boolean accept(File dir, String name) {
+					boolean isAccepted = false;
+					for (String ext : formats) {
+						if (name.indexOf(ext) != -1) {
+							isAccepted = true;
+							break;
+						}
+					}
+					return isAccepted;
+				}
+			});
+		}
+		fd.setVisible(true);
+		if (fd.getFile() != null) {
+			fileID = fd.getFile();
+			fileID = fd.getDirectory() + fileID;
+		}
+		return fileID;
+	}
 }
