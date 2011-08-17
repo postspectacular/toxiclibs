e44151c42f1bfb0d4e63265e0d5c0ba2caaf5557
postspectacular <none@none>
2011-08-17 11:00:11 +0100

    adding UV texture coordinate generation to NurbsMeshCreator, making class non-static

--- src.core/toxi/geom/nurbs/NurbsMeshCreator.java
+++ src.core/toxi/geom/nurbs/NurbsMeshCreator.java
@@ -1,25 +1,42 @@
 package toxi.geom.nurbs;
 
+import toxi.geom.Vec2D;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.Mesh3D;
 import toxi.geom.mesh.TriangleMesh;
 
 public class NurbsMeshCreator {
 
-    public static Mesh3D createControlMesh(NurbsSurface surf, Mesh3D mesh) {
+    private NurbsSurface surf;
+    private Vec2D maxUV;
+
+    public NurbsMeshCreator(NurbsSurface surf) {
+        this(surf, new Vec2D(1, 1));
+    }
+
+    public NurbsMeshCreator(NurbsSurface surf, Vec2D maxUV) {
+        this.surf = surf;
+        this.maxUV = maxUV;
+    }
+
+    public Mesh3D createControlMesh(Mesh3D mesh) {
         Vec3D[] prev = null;
         if (mesh == null) {
             mesh = new TriangleMesh();
         }
         int resU = surf.getControlNet().uLength();
         int resV = surf.getControlNet().vLength();
+        Vec2D dUV = maxUV.scale(1f / resU, 1f / resV);
         for (int u = 0; u < resU; u++) {
             Vec3D[] curr = new Vec3D[resV + 1];
             for (int v = 0; v < resV; v++) {
                 Vec3D vert = surf.getControlNet().get(u, v).to3D();
                 if (v > 0 && u > 0) {
-                    mesh.addFace(curr[v - 1], vert, prev[v - 1]);
-                    mesh.addFace(vert, prev[v], prev[v - 1]);
+                    mesh.addFace(curr[v - 1], vert, prev[v - 1],
+                            dUV.scale(u, v - 1), dUV.scale(u, v),
+                            dUV.scale(u - 1, v - 1));
+                    mesh.addFace(vert, prev[v], prev[v - 1], dUV.scale(u, v),
+                            dUV.scale(u - 1, v), dUV.scale(u - 1, v - 1));
                 }
                 curr[v] = vert;
             }
@@ -29,8 +46,7 @@ public class NurbsMeshCreator {
         return mesh;
     }
 
-    public static Mesh3D createMesh(NurbsSurface surf, Mesh3D mesh, int resU,
-            int resV, boolean isClosed) {
+    public Mesh3D createMesh(Mesh3D mesh, int resU, int resV, boolean isClosed) {
         final KnotVector knotU = surf.getUKnotVector();
         final KnotVector knotV = surf.getVKnotVector();
         double iresU = knotU.get(knotU.length() - 1) / resU;
@@ -40,6 +56,7 @@ public class NurbsMeshCreator {
         if (mesh == null) {
             mesh = new TriangleMesh();
         }
+        Vec2D dUV = maxUV.scale(1f / resU, 1f / resV);
         for (int u = 0; u <= resU; u++) {
             Vec3D[] curr = new Vec3D[resV + 1];
             for (int v = 0; v <= resV; v++) {
@@ -51,8 +68,11 @@ public class NurbsMeshCreator {
                     vert = surf.pointOnSurface(u * iresU, v * iresV);
                 }
                 if (v > 0 && u > 0) {
-                    mesh.addFace(curr[v - 1], vert, prev[v - 1]);
-                    mesh.addFace(vert, prev[v], prev[v - 1]);
+                    mesh.addFace(curr[v - 1], vert, prev[v - 1],
+                            dUV.scale(u, v - 1), dUV.scale(u, v),
+                            dUV.scale(u - 1, v - 1));
+                    mesh.addFace(vert, prev[v], prev[v - 1], dUV.scale(u, v),
+                            dUV.scale(u - 1, v), dUV.scale(u - 1, v - 1));
                 }
                 curr[v] = vert;
             }
@@ -63,5 +83,21 @@ public class NurbsMeshCreator {
         }
         mesh.computeVertexNormals();
         return mesh;
+    }
+
+    public NurbsSurface getSurface() {
+        return surf;
+    }
+
+    public Vec2D getUVScale() {
+        return maxUV;
+    }
+
+    public void setSurface(NurbsSurface surf) {
+        this.surf = surf;
+    }
+
+    public void setUVScale(Vec2D maxUV) {
+        this.maxUV = maxUV;
     }
 }
