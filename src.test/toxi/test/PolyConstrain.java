899285cafb82ffb1205f72c91b4d569d9f0910cb
postspectacular <none@none>
2011-09-19 12:48:38 +0100

    adding PolyConstrain test applet

---
+++ src.test/toxi/test/PolyConstrain.java
@@ -0,0 +1,50 @@
+package toxi.test;
+
+import processing.core.PApplet;
+import toxi.geom.Circle;
+import toxi.geom.Polygon2D;
+import toxi.geom.Vec2D;
+import toxi.processing.ToxiclibsSupport;
+import toxi.util.DateUtils;
+
+public class PolyConstrain extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main(new String[] {
+            "toxi.test.PolyConstrain"
+        });
+    }
+
+    private ToxiclibsSupport gfx;
+
+    private boolean doSave;
+
+    private Polygon2D poly;
+
+    public void draw() {
+        background(255);
+        noFill();
+        gfx.polygon2D(poly);
+        fill(255, 0, 0);
+        gfx.circle(new Vec2D(mouseX, mouseY).constrain(poly), 10);
+        if (doSave) {
+            saveFrame("PolyConstrain-" + DateUtils.timeStamp() + ".png");
+            doSave = false;
+        }
+    }
+
+    public void keyPressed() {
+        switch (key) {
+            case ' ':
+                doSave = true;
+                break;
+        }
+    }
+
+    public void setup() {
+        size(1280, 720, OPENGL);
+        gfx = new ToxiclibsSupport(this);
+        poly = new Circle(200).toPolygon2D(9).translate(width / 2, height / 2);
+        poly.get(0).x *= 0.85f;
+    }
+}
